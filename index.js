bcda8bfdbed478b006bfe9cfa8a7c8ee99aee7a1
Michle99 <sammiyonyx@gmail.com>
2023-10-19 16:37:52 -0400

    Code Refactoring: created findAssignmentByID to find valid assignments.

--- index.js
+++ index.js
@@ -78,85 +78,95 @@ const LearnerSubmissions = [
 
 
 // Learner Data function to get user data
-function getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions) {
-  const courseID = CourseInfo.id;
-  const assignmentGroupID = AssignmentGroup.id;
-  const assignments = AssignmentGroup.assignments;
-  const groupWeight = AssignmentGroup.group_weight;
-  const assignmentScores = {};
-
-  const learnerData = {};
-
-  // Check submissions and find date of submitted assignments
-  for (const submission of LearnerSubmissions) {
-    const learnID = submission.learner_id;
-    const assignmentID = submission.assignment_id;
-    const submissionDate = new Date(submission.submission.submitted_at);
-    
-    // Find the assignment with specified ID
-    const assignment = assignments.find((assignment) => assignment.id === assignmentID);
-
-    if (!assignment) {
-      console.warn(`Assignment with ID ${assignmentID} not found. Skipping submission.`);
-      continue; // Skip this submission if the assignment is not found.
-    }
-
-    const dueDate = new Date(assignment.due_at);
-
-    if (courseID !== AssignmentGroup.course_id) {
-      throw new Error("Invalid input: AssignmentGroup does not belong to the course.");
-    }
-
-    if (submissionDate > dueDate) {
-      const score = submission.submission.score;
-      const pointsPossible = assignment.points_possible;
-    
-      if (pointsPossible === 0) {
-        throw new Error("Invalid input: points_possible cannot be 0.");
-      }
-
-      if (typeof score !== "number" || isNaN(score)) {
-        throw new Error("Invalid input: Submission score must be a number.");
-      }
-
-      if (!learnerData[learnID]) {
-        learnerData[learnID] = {
-          id: learnID,
-          totalScore: 0,
-          totalWeight: 0,
-        };
-      }
-
-      learnerData[learnID].totalScore += (score / pointsPossible) * pointsPossible;
-      learnerData[learnID].totalWeight += pointsPossible;
-      assignmentScores[assignmentID] = (score / pointsPossible) * 100;
-
-    }
-  }
-
-  const results = [];
-
-  for (const learnerID in learnerData) {
-    const learner = learnerData[learnerID];
-    const weightedAverage = (learner.totalScore / learner.totalWeight) * 100;
-
-    const learnerResult = {
-      id: learner.id,
-      avg: weightedAverage,
-    }
-
-    for (const assignment of assignments) {
-      if (assignmentScores[assignment.id]) {
-        learnerResult[assignment.id] = assignmentScores[assignment.id];
-      }
-    }
-
-    results.push(learnerResult);
-  }
-
-  return results;
+function findAssignmentByID(assignments, assignmentID) {
+  return assignments.find((assignment) => assignment.id === assignmentID);
 }
 
+// function calculateWeightedAverage(totalScore, totalWeight) {
+//   return (totalScore / totalWeight) * 100;
+// }
 
-const result = getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions);
-console.log(result);
+// function processSubmission(learnerData, assignmentScores, submission, assignments) {
+//   const learnID = submission.learner_id;
+//   const assignmentID = submission.assignment_id;
+//    const courseID = CourseInfo.id;
+//   const submissionDate = new Date(submission.submission.submitted_at);
+
+//   const assignment = findAssignmentByID(assignments, assignmentID);
+
+//   if (!assignment) {
+//     console.warn(`Assignment with ID ${assignmentID} not found. Skipping submission.`);
+//     return; // Skip this submission if the assignment is not found.
+//   }
+
+//   const dueDate = new Date(assignment.due_at);
+
+//   if (courseID !== AssignmentGroup.course_id) {
+//     throw new Error("Invalid input: AssignmentGroup does not belong to the course.");
+//   }
+
+//   if (submissionDate > dueDate) {
+//     const score = submission.submission.score;
+//     const pointsPossible = assignment.points_possible;
+
+//     if (pointsPossible === 0) {
+//       throw new Error("Invalid input: points_possible cannot be 0.");
+//     }
+
+//     if (typeof score !== "number" || isNaN(score)) {
+//       throw new Error("Invalid input: Submission score must be a number.");
+//     }
+
+//     if (!learnerData[learnID]) {
+//       learnerData[learnID] = {
+//         id: learnID,
+//         totalScore: 0,
+//         totalWeight: 0,
+//       };
+//     }
+
+//     learnerData[learnID].totalScore += (score / pointsPossible) * pointsPossible;
+//     learnerData[learnID].totalWeight += pointsPossible;
+//     assignmentScores[assignmentID] = (score / pointsPossible) * 100;
+//   }
+// }
+
+// function getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions) {
+ 
+//   const assignmentGroupID = AssignmentGroup.id;
+//   const assignments = AssignmentGroup.assignments;
+//   const groupWeight = AssignmentGroup.group_weight;
+//   const assignmentScores = {};
+
+//   const learnerData = {};
+
+//   // Check submissions and find date of submitted assignments
+//   for (const submission of LearnerSubmissions) {
+//     processSubmission(learnerData, assignmentScores, submission, assignments);
+//   }
+
+//   const results = [];
+
+//   for (const learnerID in learnerData) {
+//     const learner = learnerData[learnerID];
+//     const weightedAverage = calculateWeightedAverage(learner.totalScore, learner.totalWeight);
+
+//     const learnerResult = {
+//       id: learner.id,
+//       avg: weightedAverage,
+//     };
+
+//     for (const assignment of assignments) {
+//       if (assignmentScores[assignment.id]) {
+//         learnerResult[assignment.id] = assignmentScores[assignment.id];
+//       }
+//     }
+
+//     results.push(learnerResult);
+//   }
+
+//   return results;
+// }
+
+// const result = getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions);
+// console.log(result);
